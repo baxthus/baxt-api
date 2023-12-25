630d0effba73a50432889b6f70dbf7347922325e
baxthus <baxthus@proton.me>
2023-12-25 01:16:01 -0300

    i don't know anymore

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"baxt-api/internal/env"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 var app = fiber.New()
@@ -17,6 +20,14 @@ func init() {
 }
 
 func main() {
+	app.Use(
+		cors.New(),
+		compress.New(),
+		recover.New(),
+	)
+
+	app.Static("/", "/public")
+
 	log.Fatal(app.Listen(":" + env.Port))
 }
 
